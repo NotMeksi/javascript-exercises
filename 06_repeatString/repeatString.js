8f1c46468bfba3896bdf41c416373ef70c59983b
notmeksi <its.royalekelly@gmail.com>
2026-03-11 12:37:34 +0100

    finish 06 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repword = "";
+    for (let i = 0; i < num; i++) {
+        repword += str;
+    }
+    return repword;
 };
 
 // Do not edit below this line
