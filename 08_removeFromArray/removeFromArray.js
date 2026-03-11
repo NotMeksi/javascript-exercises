afb50f7da9ac733d61cbf04a78ff6bb2f8ce9840
notmeksi <its.royalekelly@gmail.com>
2026-03-11 14:38:02 +0100

    finish 08 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let num of arr) {
+        for (let arg of args) {
+            if (num === arg) {
+                arr.splice(arr.indexOf(num), 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
